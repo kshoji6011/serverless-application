f33176942c0f4d33b15e801087f3cc57e8892c22
Kohei Shoji <koheishoji1106@gmail.com>
2021-07-29 00:26:46 +0900

    ADD: login_required

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -1,10 +1,13 @@
+from logging import log
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 from flask_blog.models.entries import Entry
+from flask_login import login_required
 from datetime import date, datetime
 
 
 @app.route('/')
+@login_required
 def show_entries():
     # 全ての記事を表示
     entries = Entry.scan()
@@ -12,6 +15,7 @@ def show_entries():
     return render_template('entries/index.html', entries=entries)
 
 @app.route('/entries', methods=['POST'])
+@login_required
 def add_entry():
     # 記事の作成処理を実装
     entry = Entry(
@@ -23,23 +27,27 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 @app.route('/entries/new', methods=['GET'])
+@login_required
 def new_entry():
     # 記事の入力フォームを表示
     return render_template('entries/new.html')
 
 @app.route('/entries/<int:id>', methods=['GET'])
+@login_required
 def show_entry(id):
     # 記事を習得
     entry = Entry.get(id)
     return render_template('entries/show.html', entry=entry)
 
 @app.route('/entries/<int:id>/edit', methods=['GET'])
+@login_required
 def edit_entry(id):
     # 記事の編集フォームを表示
     entry = Entry.get(id)
     return render_template('entries/edit.html', entry=entry)
 
 @app.route('/entries/<int:id>/update', methods=['POST'])
+@login_required
 def update_entry(id):
     # 記事の更新処理を実装
     entry = Entry.get(id)
@@ -49,6 +57,7 @@ def update_entry(id):
     return redirect(url_for('show_entries'))
 
 @app.route('/entries/<int:id>/delete', methods=['POST'])
+@login_required
 def delete_entry(id):
     # 記事の更新処理を実装
     entry = Entry.get(id)
